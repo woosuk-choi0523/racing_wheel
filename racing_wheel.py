2d767e6bd9f5e928e2b2db259ec264673380dcdc
woosuk-choi0523 <61082292+woosuk-choi0523@users.noreply.github.com>
2022-02-21 11:29:23 +0900

    change many things

--- racing_wheel.py
+++ racing_wheel.py
@@ -8,14 +8,14 @@ ser = serial.Serial('COM3', 9600) # 'COM3' is arduino`s serial port`
 gamepad = vg.VX360Gamepad()       # Starting vgamepad
 
 while True:
-    if ser.readable():  
-        temp = str(ser.readline())
-        raw_data = temp[2:-5].split()
-        stearing = (float(re.sub(r'[^0-9]', '', raw_data[0]))/102300 -0.5)*2
+    if ser.readable():
+        temp = str(ser.readline())      #Saving Data
+        raw_data = temp[2:-5].split()   #Split Data
+        stearing = (float(re.sub(r'[^0-9]', '', raw_data[0]))/102300 -0.5)*2 #calculate Data
         breakbtn = (float(re.sub(r'[^0-9]', '', raw_data[1]))-1)*-1
         accelerate = (float(re.sub(r'[^0-9]', '', raw_data[2]))-1)*-1
-        gamepad.left_joystick_float(x_value_float=stearing, y_value_float=0)
+        gamepad.left_joystick_float(x_value_float=stearing, y_value_float=0) #Send Data
         gamepad.left_trigger_float(value_float=breakbtn)
         gamepad.right_trigger_float(value_float=accelerate)
-        gamepad.update()
+        gamepad.update()        # Updata Data
         print(stearing , breakbtn,accelerate)
